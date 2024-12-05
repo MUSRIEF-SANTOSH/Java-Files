2d62e4ddf2be774c965b5493021c7375850a4046
MUSRIEF-SANTOSH <musriefsanthosh@gmail.com>
2024-12-05 11:44:33 +0530

    First Commit : Greeting Program is uploaded successfully

---
+++ Greeting/src/Main.java
@@ -0,0 +1,14 @@
+import java.lang.*;      // * imports all the codes in those package lang
+class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("welcome to java environment");
+        System.out.println("come regularly, code consisently, enhance it wisely");
+        System.out.println("Solve Via BruteForce Method First");
+        System.out.println("write pseudocode give flowchart for every project");
+        System.out.println("Tip : Avoid Deep Involvement at beginning stage, take a breaks");
+        System.out.println("What Next ? Let us Start, Meet Again");
+    }
+}
